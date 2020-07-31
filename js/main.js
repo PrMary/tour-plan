d71ad4fffdf3021093d58443d555c140982cff40
PrMary <Rysmary@yandex.ru>
2020-07-31 23:41:07 +0700

    fixed: esc close modalwindow

--- js/main.js
+++ js/main.js
@@ -57,4 +57,14 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+
+  $(document).on("keydown", function (e) {
+    console.log("Клик по кнопке Esc");
+    if (e.keyCode == 27) {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
+  });
 });
